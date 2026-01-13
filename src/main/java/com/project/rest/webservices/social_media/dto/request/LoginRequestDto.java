30a0c5ee74e0f6bf15ea4182c056e6559e025f3f
sayansen1711 <sayansen1711@gmail.com>
2026-01-13 23:28:02 +0530

    code added

--- src/main/java/com/project/rest/webservices/social_media/dto/request/LoginRequestDto.java
+++ src/main/java/com/project/rest/webservices/social_media/dto/request/LoginRequestDto.java
@@ -1,4 +1,22 @@
 package com.project.rest.webservices.social_media.dto.request;
 
 public class LoginRequestDto {
+    private String username;
+    private String password;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
--- src/main/java/com/project/rest/webservices/social_media/dto/request/SignUpRequestDto.java
+++ src/main/java/com/project/rest/webservices/social_media/dto/request/SignUpRequestDto.java
@@ -1,4 +1,22 @@
 package com.project.rest.webservices.social_media.dto.request;
 
 public class SignUpRequestDto {
+    private String usernsme;
+    private String password;
+
+    public String getUsernsme() {
+        return usernsme;
+    }
+
+    public void setUsernsme(String usernsme) {
+        this.usernsme = usernsme;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
